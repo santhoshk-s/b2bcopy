1fe2f0e544822c75e17181e4ffe610f42f0c9404
santhoshKumarsk0 <santhoshkumar.u.ssk@gmail.com>
2024-09-03 10:26:05 +0530

    backend url

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { Server } from "socket.io"; // Updated import
+import { Server } from "socket.io";
+import cors from "cors";
 
 // Utilities 
 import connectDB from "./config/db.js";
@@ -11,39 +12,50 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import  messageRoute from './routes/messagesRoute.js';
-import ChatuserRouts from './routes/chatuserRoutes.js'
-dotenv.config();
-const port = 5001;
+import messageRoute from './routes/messagesRoute.js';
+import chatUserRoutes from './routes/chatuserRoutes.js';
 
+dotenv.config();
 connectDB();
 
 const app = express();
+const port = 5001;
+
+// CORS configuration
+app.use(cors({
+  origin: 'https://b2b-frontendcopy.netlify.app', // Allow your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow the necessary HTTP methods
+  credentials: true, // If you're sending cookies or authorization headers
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// API Routes
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/messages", messageRoute);
-app.use("/api/auth", ChatuserRouts);
+app.use("/api/auth", chatUserRoutes);
+
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
+// Serve static files
 const dirname = path.resolve();
 app.use("/uploads", express.static(path.join(dirname, "/uploads")));
 
+// Start the server
 const server = app.listen(port, () => console.log(`Server running on port: ${port}`));
 
-// Use Server instead of socket
+// Configure Socket.io with CORS
 const io = new Server(server, {
   cors: {
-    // origin: SERVER_URL,
+    origin: 'https://b2b-frontendcopy.netlify.app/login',
     credentials: true,
   },
 });
@@ -53,6 +65,7 @@ let onlineUserIds = {};
 
 io.on("connection", (socket) => {
   global.chatSocket = socket;
+
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
     onlineUserIds[userId] = true;
